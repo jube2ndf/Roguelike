78c2f9e1bf2432e4bb5cdb52c12a999b476d54f2
Georgiy Burnaev <jubendf@gmail.com>
2026-05-22 16:59:25 +0400

    Create game engine

---
+++ Rougelike/Engine/Engine.cpp
@@ -0,0 +1,18 @@
+#include "pch.h"
+#include "Engine.h"
+#include <iostream>
+
+Engine::Engine()
+{
+	
+}
+
+void Engine::Initialize()
+{
+	std::cout << "Engine initialized\n";
+}
+
+void Engine::Run()
+{
+	std::cout << "Engine run\n";
+}
