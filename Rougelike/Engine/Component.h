f3fc825a87bd19974451cb2ffdfcaab1858111c6
Georgiy Burnaev <jubendf@gmail.com>
2026-05-27 21:14:27 +0400

    Add Component

---
+++ Rougelike/Engine/Component.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <iostream>
+namespace Engine {
+	class GameObject;
+
+	class Component
+	{
+	public:
+		Component(GameObject* gameObject):_owner(gameObject) {
+
+		}
+		virtual ~Component() {
+			std::cout << "Deleted component: " << this << std::endl;
+		}
+
+		virtual void Update(float deltaTime) {};
+
+		GameObject* GetGameObject() const {
+			return this->_owner;
+		}
+	protected:
+		GameObject* _owner;
+	};
+}
