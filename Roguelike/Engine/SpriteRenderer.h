d3c4b91db63ac48a4c3b1f25fe876484955959e6
Georgiy Burnaev <jubendf@gmail.com>
2026-05-29 09:42:00 +0400

    Add Sprite render

---
+++ Roguelike/Engine/SpriteRenderer.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <SFML/Graphics/Sprite.hpp>
+#include "Component.h"
+namespace GameEngine {
+    class SpriteRenderer :
+        public Component
+    {
+    public:
+        SpriteRenderer(GameObject* owner, 
+            const sf::Texture& texture) 
+            :Component(owner),
+            _sprite(texture)
+        {
+            
+        }
+
+
+    protected:
+        sf::Sprite _sprite;
+
+        int sortingLayer = 0;
+    };
+}
