dc1b9d386531e4b92a8464fe1d3ac54175513d5f
Georgiy Burnaev <jubendf@gmail.com>
2026-05-29 15:26:43 +0400

    auto assembly & Add Riginbody

---
+++ Roguelike/Engine/Rigidbody.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "Component.h"
+
+namespace GameEngine {
+    class Rigidbody :
+        public Component
+    {
+    public:
+        sf::Vector2f velocity = { 0.f, 0.f };
+        sf::Vector2f acceleration = { 0.f, 0.f };
+
+        float mass = 1.f;
+        float drag = 8.f;
+
+        bool isKinematic = false;
+        Rigidbody(GameObject* owner): Component(owner) {}
+    };
+}
