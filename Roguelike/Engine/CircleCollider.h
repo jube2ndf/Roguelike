47f4ddee673a0784b6fc32e3ff6976ac9271b054
Georgiy Burnaev <jubendf@gmail.com>
2026-05-30 15:12:21 +0400

    CircleCollider class implementation

--- Roguelike/Engine/CircleCollider.h
+++ Roguelike/Engine/CircleCollider.h
@@ -1,1 +1,13 @@
 #pragma once
+#include "Collider.h"
+
+namespace GameEngine {
+	class CircleCollider :
+		public Collider {
+		CircleCollider(GameObject* owner)
+			:Collider(owner)
+		{}
+
+		float radius = 25.f;
+	};
+}
