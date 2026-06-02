35664f8d3f1d92d0eb4353f1091ce4341a18c281
Georgiy Burnaev <jubendf@gmail.com>
2026-06-02 13:11:58 +0400

    Fix: Render sprite

--- Roguelike/Engine/RenderSystem.cpp
+++ Roguelike/Engine/RenderSystem.cpp
@@ -5,31 +5,52 @@
 #include "GameObject.h"
 #include "TransformComponent.h"
 #include "ShapeRenderer.h"
+#include "SpriteRenderer.h"
 
 void GameEngine::RenderSystem::Render(sf::RenderWindow& window, Scene* scene)
 {
     window.clear();
+
     for (auto& object : scene->GetObjects())
     {
         auto transform =
             object->GetComponent<TransformComponent>();
 
-        auto renderer =
-            object->GetComponent<ShapeRenderer>();
-
-        if (!transform || !renderer)
+        if (!transform)
             continue;
 
-        renderer->shape->setPosition(
-            transform->GetWorldPosition());
+        // ===== SHAPES =====
+        if (auto shapeRenderer =
+            object->GetComponent<ShapeRenderer>())
+        {
+            shapeRenderer->shape->setPosition(
+                transform->GetWorldPosition());
 
-        renderer->shape->setRotation(
-            sf::degrees(transform->GetWorldRotation()));
+            shapeRenderer->shape->setRotation(
+                sf::degrees(transform->GetWorldRotation()));
 
-        renderer->shape->setScale(
-            transform->GetWorldScale());
+            shapeRenderer->shape->setScale(
+                transform->GetWorldScale());
 
-        window.draw(*renderer->shape);
+            window.draw(*shapeRenderer->shape);
+        }
+
+        // ===== SPRITES =====
+        if (auto spriteRenderer =
+            object->GetComponent<SpriteRenderer>())
+        {
+            spriteRenderer->GetSprite().setPosition(
+                transform->GetWorldPosition());
+
+            spriteRenderer->GetSprite().setRotation(
+                sf::degrees(transform->GetWorldRotation()));
+
+            spriteRenderer->GetSprite().setScale(
+                transform->GetWorldScale());
+
+            window.draw(spriteRenderer->GetSprite());
+        }
     }
+
     window.display();
 }
