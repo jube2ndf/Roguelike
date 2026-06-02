2c50c7754016e9073557fab1323892fc002cd23a
Georgiy Burnaev <jubendf@gmail.com>
2026-06-02 11:17:22 +0400

    Add: constructor

--- Roguelike/Roguelike/PlayerMovementComponent.h
+++ Roguelike/Roguelike/PlayerMovementComponent.h
@@ -1,10 +1,13 @@
 #pragma once
 #include <Component.h>
+#include <GameObject.h>
 namespace Roguelike {
     class PlayerMovementComponent :
         public GameEngine::Component
     {
     public:
+        PlayerMovementComponent(GameEngine::GameObject* owner):
+            GameEngine::Component(owner){}
         void Update(float dt) override;
 
         float speed = 200.f;
