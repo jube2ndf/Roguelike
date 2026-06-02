400c8ec5c06b7e30d358894b4f8280c16c6e6581
Georgiy Burnaev <jubendf@gmail.com>
2026-06-02 13:17:24 +0400

    test wall

--- Roguelike/Roguelike/Game.cpp
+++ Roguelike/Roguelike/Game.cpp
@@ -22,8 +22,8 @@ void Roguelike::Game::CreateLevel1()
         this->_engine->GetSceneManager()
         .CreateScene("Level1");
     Player::Create(scene, { 1280/2, 720/2 });
-    Wall::CreateWall(scene, { 0, 0 }, { 800, 20 });   // верх
-    Wall::CreateWall(scene, { 0, 580 }, { 800, 20 }); // низ
-    Wall::CreateWall(scene, { 0, 0 }, { 20, 600 });   // лево
-    Wall::CreateWall(scene, { 780, 0 }, { 20, 600 }); // право
+    Wall::CreateWall(scene, { 0, 0 }, { 80, 20 });   // верх
+    //Wall::CreateWall(scene, { 0, 580 }, { 800, 20 }); // низ
+    //Wall::CreateWall(scene, { 0, 0 }, { 20, 600 });   // лево
+    //Wall::CreateWall(scene, { 780, 0 }, { 20, 600 }); // право
 }
