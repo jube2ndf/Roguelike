e43d43bbcdb91fea41c3c0827267b37ae19033eb
Georgiy Burnaev <jubendf@gmail.com>
2026-06-05 21:09:43 +0400

    Add: simple healthcomponent

---
+++ Roguelike/Roguelike/HealthComponent.h
@@ -0,0 +1,49 @@
+#pragma once
+#include <Component.h>
+#include <GameObject.h>
+#include <algorithm>
+
+namespace Roguelike {
+	class HealthComponent:
+		public GameEngine::Component
+	{
+	public:
+		HealthComponent(GameEngine::GameObject* owner, float baseHealth)
+			:GameEngine::Component(owner),
+			baseMaxHealth(baseHealth),
+			bonusMaxHealth(0),
+			currentHealth(baseHealth)
+		{
+
+		}
+
+		void TakeDamage(float damage)
+		{
+			currentHealth =
+				std::max<float>(0.f,
+					currentHealth - damage);
+		}
+
+		float GetMaxHealth() const
+		{
+			return baseMaxHealth + bonusMaxHealth;
+		}
+
+		bool IsAlive() const
+		{
+			return currentHealth > 0.f;
+		}
+
+		void Heal(float amount)
+		{
+			currentHealth =
+				std::min(currentHealth + amount,
+					GetMaxHealth());
+		}
+
+	protected:
+		float baseMaxHealth;
+		float bonusMaxHealth;
+		float currentHealth;
+	};
+}
