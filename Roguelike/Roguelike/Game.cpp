a0bbddef4312533c27b5d9dbdec236c5e6bfaec5
Georgiy Burnaev <jubendf@gmail.com>
2026-06-07 11:53:22 +0400

    Add walls

--- Roguelike/Roguelike/Game.cpp
+++ Roguelike/Roguelike/Game.cpp
@@ -28,7 +28,9 @@ void Roguelike::Game::CreateLevel1()
         this->_engine->GetSceneManager()
         .CreateScene("Level1");
     Player::Create(scene, { 1280/2, 720/2 });
-    Wall::CreateWall(scene, { 0, 0 }, { 20, 600 });
-    Wall::CreateWall(scene, { 780, 0 }, { 20, 600 });
+    Wall::CreateWall(scene, { 1280 / 2, 0 }, { 1280, 40 });
+    Wall::CreateWall(scene, { 1280 / 2, 720 }, { 1280, 40 });
+    Wall::CreateWall(scene, { 0, 720/2 }, { 40, 720 });
+    Wall::CreateWall(scene, { 1280, 720 / 2 }, { 40, 720 });
     Enemy::Create(scene, { 1100, 600 });
 }
